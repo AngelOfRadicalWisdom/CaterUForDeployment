a7c15846acfb5ab69b8a5193ab021640c5927d6c
Ma. Christina Tolcidas <31025996+AngelOfRadicalWisdom@users.noreply.github.com>
2020-06-19 22:36:07 +0800

    apriorisettings=>aprioriSettings

--- app/AprioriSettings.php
+++ app/AprioriSettings.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class AprioriSettings extends Model
 {
     protected $primaryKey = 'id';
-    protected $table = 'apriorisettings';
+    protected $table = 'aprioriSettings';
     public $incrementing= true;
     public $timestamps = false;
     protected $fillable = array('id','support','confidence');
