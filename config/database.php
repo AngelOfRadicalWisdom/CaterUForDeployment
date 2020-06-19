cb12738f329c774649440c137de448a753d2aa79
Ma. Christina Tolcidas <31025996+AngelOfRadicalWisdom@users.noreply.github.com>
2020-06-19 17:21:48 +0800

    update database

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'db4free.net'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT','3306'),
             'database' => env('DB_DATABASE', 'cateruweb'),
             'username' => env('DB_USERNAME', 'queenaki'),
             'password' => env('DB_PASSWORD', 'usjr12345'),
