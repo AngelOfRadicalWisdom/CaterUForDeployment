481271cc85908cca6772d4f1a2c85323502ee016
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-09 23:12:15 +0800

    p33

--- app/Http/Controllers/TemporaryTableController.php
+++ app/Http/Controllers/TemporaryTableController.php
@@ -306,20 +306,21 @@ class TemporaryTableController extends Controller
 
     public function getBarKitchenOrders(){
         $orders = DB::table('kitchenrecords')->get();
+        $bundles = [];
         $kitchen = [];
         $details=[];
         $bar = [];
 
         foreach($orders as $order){
             if( $order->bundleid != null  && $order->menuID ==null ){
-                $bundles = $this->getMealBundles($order->bundleid);
-                foreach($bundles as $b){
-                    array_push($details,array(
-                        "itemName" => $b->name,
-                        "menuID" =>$b->menuID,
-                        "qty"=>$b->qty
-                    ));
-                }
+                array_push($bundles,($this->getMealBundles($order->bundleid)));
+                // foreach($bundles as $b){
+                //     array_push($details,array(
+                //         "itemName" => $b->name,
+                //         "menuID" =>$b->menuID,
+                //         "qty"=>$b->qty
+                //     ));
+                // }
 
             }
         }
