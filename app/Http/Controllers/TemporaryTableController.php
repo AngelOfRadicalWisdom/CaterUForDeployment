482495d28f8c63feb42cc0a21897dd6f996b911f
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-10 13:15:42 +0800

    p87

--- app/Http/Controllers/TemporaryTableController.php
+++ app/Http/Controllers/TemporaryTableController.php
@@ -433,7 +433,7 @@ class TemporaryTableController extends Controller
         ->join('sub_categories','menus.subcatid','=','sub_categories.subcatid')
         ->join('categories','categories.categoryid','=','sub_categories.categoryid')
         ->where('categories.categoryname','=','Drinks')
-        ->whereOr('categories.categoryname','=','Dessert')
+        ->orWhere('categories.categoryname','=','Dessert')
         ->where('menus.menuID',$menuid)
         ->get();
     }
