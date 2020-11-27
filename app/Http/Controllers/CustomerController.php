cb94ecee0cca738360af8bcf1818d6fa4dbe8ae6
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-27 19:28:10 +0800

    20

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -191,14 +191,14 @@ class CustomerController extends Controller
                    $kitchenorders->status = 'waiting';
                    $kitchenorders->save();
 
-                //    $tempOrders = new TemporaryOrders();
-                //    $tempOrders->id = $kitchenorders->id;
-                //    $tempOrders->orderQty = $value['orderQty'];
-                //    $tempOrders->menuID = $item->menuID;
-                //    $tempOrders->bundleid = $item->bundleid;
-                //    $tempOrders->order_id = $order_id;
-                //    $tempOrders->status = 'waiting';
-                //    $tempOrders->save();
+                   $tempOrders = new TemporaryOrders();
+                   $tempOrders->id = $kitchenorders->id;
+                   $tempOrders->orderQty = $value['orderQty'];
+                   $tempOrders->menuID = $item->menuID;
+                   $tempOrders->bundleid = $item->bundleid;
+                   $tempOrders->order_id = $order_id;
+                   $tempOrders->status = 'waiting';
+                   $tempOrders->save();
                    
                }
             }else{
