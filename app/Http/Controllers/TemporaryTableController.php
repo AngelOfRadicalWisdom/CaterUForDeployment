b43d6591a245fbab2651fc19c7b8efa68c71ce57
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-29 17:41:46 +0800

    68

--- app/Http/Controllers/TemporaryTableController.php
+++ app/Http/Controllers/TemporaryTableController.php
@@ -213,7 +213,7 @@ class TemporaryTableController extends Controller
         ->where('temporary_orders.status','pendingcancel')->get();
 
         return response()->json([
-           $orders,
+            'tablenos'=>$orders,
             'reason'=>'Order item cancellation'
         ]);
     }
