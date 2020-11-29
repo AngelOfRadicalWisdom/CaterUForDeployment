7ea7671da135e986aaa7f3f18844a34a89516634
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-29 23:00:50 +0800

    86

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -95,7 +95,7 @@ class OrderDetailController extends BaseController
                    ->join('categories','categories.categoryid','=','sub_categories.categoryid')
                    ->where('bundles.bundleid',$bundleid)
                    ->get();
-        return  $kitchen[0]->bundleName; 
+        return  $kitchen[0]->qty; 
     }
     
 
