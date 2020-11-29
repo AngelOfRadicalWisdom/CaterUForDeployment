8de8b0a0168400ffdb7d26d3f73a09b45c6ced42
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-29 21:40:53 +0800

    76

--- app/Http/Controllers/TemporaryTableController.php
+++ app/Http/Controllers/TemporaryTableController.php
@@ -222,8 +222,8 @@ class TemporaryTableController extends Controller
     public function cancelItem($id,Request $request){
         $temporaryId = $request->tempId;
         $reason = $request->reason;
-        TemporaryOrders::where("tempId",$temporaryId)->delete();
-        Kitchen::where("id",$temporaryId)->delete();
+        // TemporaryOrders::where("tempId",$temporaryId)->delete();
+        // Kitchen::where("id",$temporaryId)->delete();
         OrderDetail::where('id',$id)
         ->update(['status'=>$reason,"subtotal"=>0.0]);
 
