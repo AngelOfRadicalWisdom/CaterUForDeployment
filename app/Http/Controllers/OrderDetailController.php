09b7c9ff3660cb41e563be745efe1e1bf5626914
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-11-30 17:00:32 +0800

    90

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -188,6 +188,10 @@ class OrderDetailController extends BaseController
             $records = OrderDetail::find($request->id);
             $records->status = "served";
             $records->save();
+
+            $temp = TemporaryOrders::find($request->tempId);
+            $temp->status ="served";
+            $temp->save();
         }
 
         return response()->json([
