3e6c7c6bb6d3ceec63388a19192e5c61fae2f1ee
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-05 19:56:27 +0800

    b24

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -175,40 +175,40 @@ class OrderDetailController extends BaseController
     {
         //SINGLE ORDERS ONLY
 
-        $temp = TemporaryOrders::find($request->tempId);
-        $temp->qtyServed-=$request['noItemToServe'];
-        $temp->save();
+        $temp = TemporaryOrders::find($request->tempId)->get();
+        // $temp->qtyServed-=$request['noItemToServe'];
+        // $temp->save();
 
-        if($temp->qtyServed == 0){
-            $tempO = TemporaryOrders::find($request->tempId);
-            $tempO->status = "served";
-            $tempO->save();
-        }
+        // if($temp->qtyServed == 0){
+        //     $tempO = TemporaryOrders::find($request->tempId);
+        //     $tempO->status = "served";
+        //     $tempO->save();
+        // }
 
-        if($temp->bundleid==null){
-            $records = OrderDetail::find($temp->order_details_id);
-            $records->qtyServed -= $request['noItemToServe'];
-            $records->save();
+        // if($temp->bundleid==null){
+        //     $records = OrderDetail::find($temp->order_details_id);
+        //     $records->qtyServed -= $request['noItemToServe'];
+        //     $records->save();
 
-            if($records->qtyServed == 0 ){
-                $statusDetail = OrderDetail::find($temp->order_details_id);
-                $statusDetail->status = "served";
-                $statusDetail->save();
-            }
-        }else{
+        //     if($records->qtyServed == 0 ){
+        //         $statusDetail = OrderDetail::find($temp->order_details_id);
+        //         $statusDetail->status = "served";
+        //         $statusDetail->save();
+        //     }
+        // }else{
             
-            $bundle = DB::table('temporary_orders')
-            ->where('order_details_id',$temp->order_details_id)
-            ->where('status','waiting')
-            ->get();
+        //     $bundle = DB::table('temporary_orders')
+        //     ->where('order_details_id',$temp->order_details_id)
+        //     ->where('status','waiting')
+        //     ->get();
 
-            if(COUNT($bundle) == 0){
-                $recordsB = OrderDetail::find($temp->order_details_id);
-                $recordsB->qtyServed = 0;
-                $recordsB->status = "served";
-                $recordsB->save(); 
-            }
-        }
+        //     if(COUNT($bundle) == 0){
+        //         $recordsB = OrderDetail::find($temp->order_details_id);
+        //         $recordsB->qtyServed = 0;
+        //         $recordsB->status = "served";
+        //         $recordsB->save(); 
+        //     }
+        // }
 
         return response()->json([
             'message' => $request->noItemToServe
