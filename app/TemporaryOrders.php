e7b5c2e57a1554089839b0aced96f56199992e1f
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-05 20:07:06 +0800

    b32

--- app/TemporaryOrders.php
+++ app/TemporaryOrders.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class TemporaryOrders extends Model
 {
     protected $table = 'temporary_orders';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'tempId';
     public $incrementing= true;
     public $timestamps = true;
 
-    protected $fillable = ['tempId','id','order_id','menuID','bundleid','orderQty','status',];
+    protected $fillable = ['tempId','id','order_id','menuID','bundleid','orderQty','qtyServed','status',];
 }
