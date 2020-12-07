02e36421abb5b9bbc5f283b73765ea3ff0354242
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-07 16:40:43 +0800

    b45

--- app/Http/Controllers/TemporaryTableController.php
+++ app/Http/Controllers/TemporaryTableController.php
@@ -190,22 +190,13 @@ class TemporaryTableController extends Controller
             'message' => $message
         ]);
     }
-    // public function setServedTempOrders($tempId){
-    //     $message = '';
-    //     $tempOrders = DB::table('temporary_orders')
-    //     ->join('orders','orders.order_id','=','temporary_orders.order_id')
-    //     ->join('order_details','order_details.order_id','=','orders.order_id')
-    //     ->where('tempId',$tempId)
-
-    //     return response()->json([
-    //         'message'=> $tempOrders
-    //     ]);
-    // }
-
+  
     public function requestCancelOrderItem($id){ 
         $orders = DB::table('order_details')
         ->where('id',$id)
         ->update(['status'=>'pendingcancel']);
+
+        $tempData = DB::table('temporary_orders')->where('order_details_id',$id)->update(['status'=>'pendingcancel']);
 
         return response()->json(
             [
