2699273d40836bab92dc42270356d483f8d9282f
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-09 12:53:32 +0800

    bundle status updated - seeder

--- database/seeds/BundleMenuSeeder.php
+++ database/seeds/BundleMenuSeeder.php
@@ -18,7 +18,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=> 2,
                 'name' => 'Bundle1',
                 'details'=>'bundle 1',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 1'
             ],
@@ -28,7 +28,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>3,
                 'name' => 'Bundle2',
                 'details'=>'bundle 2',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 2'
             ],
@@ -38,7 +38,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>4,
                 'name' => 'Bundle3',
                 'details'=>'bundle 3',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 3'
             ],
@@ -48,7 +48,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>6,
                 'name' => 'Bundle4',
                 'details'=>'bundle 4',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 4'
             ],
@@ -58,7 +58,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>5,
                 'name' => 'Bundle5',
                 'details'=>'bundle 5',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 5'
             ],
@@ -68,7 +68,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>3,
                 'name' => 'Bundle6',
                 'details'=>'bundle 6',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 6'
             ],
@@ -78,7 +78,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>4,
                 'name' => 'Bundle7',
                 'details'=>'bundle 7',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 7'
             ],
@@ -88,7 +88,7 @@ class BundleMenuSeeder extends Seeder
                 'servingsize'=>5,
                 'name' => 'Bundle8',
                 'details'=>'bundle 8',
-                'status'=>'AVAILABLE',
+                'status'=>'Available',
                 'image'=>'CaterU.png',
                 'name'=>'bundle 8'
             ],
