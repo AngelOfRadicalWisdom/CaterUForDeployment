557288626df55b7de0edb2973f7d689d30d18cab
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-22 15:52:02 +0800

    22

--- app/Http/Controllers/AprioriC2Controller.php
+++ app/Http/Controllers/AprioriC2Controller.php
@@ -457,7 +457,7 @@ class AprioriC2Controller extends Controller
             ));
         }
     }
-       return response()->json(['menu' => $data]);
+       return response()->json(['menu' => $groupedData]);
 
    }
 
