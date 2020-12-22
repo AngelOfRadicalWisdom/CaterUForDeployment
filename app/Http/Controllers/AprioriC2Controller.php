bad6b0eebf8c28dd51cc03a836001bc2f8f2b8f9
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-22 15:13:41 +0800

    08

--- app/Http/Controllers/AprioriC2Controller.php
+++ app/Http/Controllers/AprioriC2Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\AprioriProcessEvent;
 use App\Helper\AprioriNew;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Menu;
 use Illuminate\Support\Facades\DB;
