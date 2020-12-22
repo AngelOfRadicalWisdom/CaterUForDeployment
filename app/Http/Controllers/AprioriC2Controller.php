7a9380ac5556142673302a56b4c58a873d43fae1
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-22 19:58:06 +0800

    66

--- app/Http/Controllers/AprioriC2Controller.php
+++ app/Http/Controllers/AprioriC2Controller.php
@@ -447,7 +447,7 @@ class AprioriC2Controller extends Controller
         ));
     }
 
-    return response()->json(['menu' => $menu]);
+    return response()->json(['menu' => $transactions]);
 
    }
 
