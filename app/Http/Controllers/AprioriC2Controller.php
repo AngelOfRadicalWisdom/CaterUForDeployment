bfe4e7f67d558f4626844b3cfd3c8d1955150831
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-22 15:26:54 +0800

    14

--- app/Http/Controllers/AprioriC2Controller.php
+++ app/Http/Controllers/AprioriC2Controller.php
@@ -417,50 +417,40 @@ class AprioriC2Controller extends Controller
         ->get());
     }
 
-// //    $transactions = DB::table('menus')
-// //    ->selectRaw('group_concat(menus.name) as name')
-// //    ->selectRaw('group_concat(menus.menuID) as menuID')
-// //    ->selectRaw('group_concat(menus.image) as image')
-// //    ->selectRaw('group_concat(menus.details) as details')
-// //    ->selectRaw('group_concat(menus.servingsize) as servingsize')
-// //    ->selectRaw('group_concat(menus.price) as price')
-// //    ->selectRaw('group_concat(menus.subcatid) as subcatid')
-// //    ->whereIn('menuID', $pairs)
-// //    ->get();
-//        foreach ($transactions as $row) {
-//         $menu[] = explode(",", $row->menuID);
-//     }
-//     for ($index = 0; $index < count($menu); $index++) {
-//         foreach ($menu[$index] as $Smenus) {
-//             if ($Smenus != $menuId) {
-//                 $groupedData[] = $Smenus;
-//             }
-//         }
-//     }
-//     $final = array_unique($groupedData);
-//     $groupedData = [];
-//     $data;
-//     foreach ($final as $row) {
-//         $data = DB::table('menus')->where('menuID', $row)->get();
-//         array_push($groupedData, $row);
-//     }
-//     $data = [];
-//     for ($i = 0; $i < count($groupedData); $i++) {
-//         $t = DB::table('menus')->where('menuID', $groupedData[$i])->get();
+    foreach ($transactions as $row) {
+        $menu[] = explode(",", $row->menuID);
+    }
+    for ($index = 0; $index < count($menu); $index++) {
+        foreach ($menu[$index] as $Smenus) {
+            if ($Smenus != $menuId) {
+                $groupedData[] = $Smenus;
+            }
+        }
+    }
+    $final = array_unique($groupedData);
+    $groupedData = [];
+    $data;
+    foreach ($final as $row) {
+        $data = DB::table('menus')->where('menuID', $row)->get();
+        array_push($groupedData, $row);
+    }
+    $data = [];
+    for ($i = 0; $i < count($groupedData); $i++) {
+        $t = DB::table('menus')->where('menuID', $groupedData[$i])->get();
 
-//         foreach ($t as $a) {
-//             array_push($data, array(
-//                 'name' => $a->name,
-//                 'menuID' => $a->menuID,
-//                 'image' => asset('/menu/menu_images/'.$a->image),
-//                 'details' => $a->details,
-//                 'price'=> $a->price,
-//                 'servingsize' => $a->servingsize,
-//                 'subcatid'=> $a->subcatid
-//             ));
-//         }
-//     }
-       return response()->json($transactions);
+        foreach ($t as $a) {
+            array_push($data, array(
+                'name' => $a->name,
+                'menuID' => $a->menuID,
+                'image' => asset('/menu/menu_images/'.$a->image),
+                'details' => $a->details,
+                'price'=> $a->price,
+                'servingsize' => $a->servingsize,
+                'subcatid'=> $a->subcatid
+            ));
+        }
+    }
+       return response()->json($data);
 
    }
 
